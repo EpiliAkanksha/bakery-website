f6053f64d22fa0b3a04485d190e1a46bb9b43761
EpiliAkanksha <epiliakanksha@gmail.com>
2026-05-18 11:16:10 +0530

    Update navigation links in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -45,13 +45,13 @@ function App() {
         <h2>Golden Crust</h2>
 
         <ul>
-          <li>Home</li>
-          <li>
-  <a href="#menu">Menu</a>
-</li>
-          <li>About</li>
-          <li>Contact</li>
-          <li>Cart ({totalItems})</li>
+          <li><a href="#">Home</a></li>
+
+          <li><a href="#menu">Menu</a></li>
+          
+          <li><a href="#about">About</a></li>
+          
+          <li><a href="#contact">Contact</a></li>
         </ul>
       </nav>
 
